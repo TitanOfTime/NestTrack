74716f3e8795a073ea58d366c092f602baec8f27
TitanOfTime <immylance@gmail.com>
2026-05-11 14:08:15 +0530

    security: remove hardcoded Firebase keys, load from .env via flutter_dotenv

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,15 +1,14 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    return const FirebaseOptions(
-      apiKey: 'AIzaSyCo8nJaoPIqs971kTDW_twLJL1kH9pv5WU',
-      appId: '1:1021354177769:web:e00e6d2e727da5365e2c78',
-      messagingSenderId: '1021354177769',
-      projectId: 'nesttrack-935ef',
-      storageBucket: 'nesttrack-935ef.firebasestorage.app',
+    return FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
     );
   }
 }
